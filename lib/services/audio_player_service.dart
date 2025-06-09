a5fbc5e40efbf9dc95cc50cf526e9e87e1f92c87
Unity <guilhermtrout@gmail.com>
2025-06-09 15:21:08 -0300

    Add music player for background

---
+++ lib/services/audio_player_service.dart
@@ -0,0 +1,50 @@
+import 'package:audioplayers/audioplayers.dart';
+
+class MusicPlayerService {
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  MusicPlayerService() {
+    _audioPlayer.setReleaseMode(ReleaseMode.loop);
+    _audioPlayer.setVolume(0.3);
+  }
+
+  Future<void> playBackgroundMusic(String assetPath) async {
+    if (!_isPlaying) {
+      try {
+        await _audioPlayer.play(AssetSource(assetPath));
+        _isPlaying = true;
+      } catch (e) {
+        print('Erro ao tocar música: $e');
+      }
+    }
+  }
+
+  Future<void> stopBackgroundMusic() async {
+    if (_isPlaying) {
+      await _audioPlayer.stop();
+      _isPlaying = false;
+    }
+  }
+
+  Future<void> pauseBackgroundMusic() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+    }
+  }
+
+  Future<void> resumeBackgroundMusic() async {
+    if (!_isPlaying) {
+      try {
+        await _audioPlayer.resume();
+        _isPlaying = true;
+      } catch (e) {
+        print('Erro ao retomar música: $e');
+      }
+    }
+  }
+
+  void dispose() {
+    _audioPlayer.dispose();
+  }
+}
